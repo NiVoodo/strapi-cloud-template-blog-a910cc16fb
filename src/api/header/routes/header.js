3c41ff0c258368a80a5fc810b6ba126f1fa3d7b6
NVoodo <leon.schmidt@esyon.de>
2025-09-10 16:24:33 +0200

    Update header.js

--- src/api/header/routes/header.js
+++ src/api/header/routes/header.js
@@ -1,9 +1,19 @@
 'use strict';
 
-/**
- * about js.
- */
-
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::header.header');
+/**
+ * Single-Type "header"
+ * Macht GET /api/header öffentlich (ohne Auth).
+ * Weitere Actions (update etc.) bleiben standardmäßig geschützt.
+ */
+module.exports = createCoreRouter('api::header.header', {
+  config: {
+    find: {
+      auth: false,
+      // optional:
+      // policies: [],
+      // middlewares: [],
+    },
+  },
+});
